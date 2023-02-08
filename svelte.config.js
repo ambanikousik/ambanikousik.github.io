c954cd856a7ab39831b84182f418d80954d2bae7
Kousik Ambani <rikousik@gmail.com>
2023-02-08 17:25:01 +0600

    new file

---
+++ svelte.config.js
@@ -0,0 +1,45 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+import { mdsvex } from 'mdsvex';
+import rehypeExternalLinks from 'rehype-external-links';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+
+const extensions = ['.svelte', '.md'];
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter()
+	},
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({
+			layout: {
+				article: './src/routes/(blog-article)/mdsvex-layout.svelte'
+			},
+			extensions: extensions,
+			rehypePlugins: [
+				rehypeExternalLinks, // Adds 'target' and 'rel' to external links
+				rehypeSlug, // Adds 'id' attributes to Headings (h1,h2,etc)
+				[
+					rehypeAutolinkHeadings,
+					{
+						// Adds hyperlinks to the headings, requires rehypeSlug
+						behavior: 'prepend',
+						properties: { className: ['heading-link'], title: 'Permalink', ariaHidden: 'true' },
+						content: {
+							type: 'element',
+							tagName: 'span',
+							properties: {},
+							children: [{ type: 'text', value: '#' }]
+						}
+					}
+				]
+			]
+		})
+	],
+	extensions: extensions
+};
+
+export default config;
